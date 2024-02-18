d3f43e8108660eabf836ced021f1e822f47db056
luisyet <sulcaluis151999@gmail.com>
2024-02-18 17:23:11 -0500

    configure cors

--- src/main/java/com/example/pokeapi/controller/PokemonController.java
+++ src/main/java/com/example/pokeapi/controller/PokemonController.java
@@ -5,6 +5,7 @@ import com.example.pokeapi.service.PokemonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ public class PokemonController {
     @Autowired
     private PokemonService pokemonService;
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping()
     public ResponseEntity<ArrayList<Pokemon>> getPokemones(){
         ArrayList<Pokemon> pokemones = this.pokemonService.getPokemones();
